995ff77cc14e3a4b5c056bf64e57e94a94e25496
Cloud User <auto@automation.canadacentral.cloudapp.azure.com>
2021-07-11 05:22:34 +0000

    main

---
+++ main.tf
@@ -0,0 +1,42 @@
+module "resource_group" {
+  source = "./modules/resource_group"
+  # rg_name    = module.resource_group.rg_name
+}
+
+module "common" {
+  source = "./modules/common"
+  # rg_name = "group1-assignment1-rg"
+  rg_name = module.resource_group.rg_name
+}
+
+module "database" {
+  source  = "./modules/database"
+  rg_name = module.resource_group.rg_name
+}
+
+module "linux" {
+  source   = "./modules/linux"
+  rg_name  = module.resource_group.rg_name
+  location = "CanadaCentral"
+  linux_name = {
+    "group1-linux-vm1" = "Standard_B1s"
+    "group1-linux-vm2" = "Standard_B1s"
+  }
+  subnet_id = module.network.subnet1
+}
+
+module "network" {
+  source   = "./modules/network"
+  rg_name  = module.resource_group.rg_name
+  location = "CanadaCentral"
+}
+
+module "vmwindows" {
+  source    = "./modules/vmwindows"
+  windows_name = {
+    "group1-win-vm1" = "Standard_B1s"
+  }
+  subnet_id = module.network.subnet1
+  rg_name   = module.resource_group.rg_name
+}
+
